91d174b7232fab18b8fccf2f838adf80dfb392ec
vvcb <vvcb.n1@gmail.com>
2024-01-29 22:42:57 +0000

    Add person, visit_occurrence and tests

---
+++ models/visit_occurrence.sql
@@ -0,0 +1,21 @@
+select
+  visit_occurrence_id,
+  person_id,
+  visit_concept_id,
+  visit_start_date,
+  visit_start_datetime,
+  visit_end_date,
+  visit_end_datetime,
+  visit_type_concept_id,
+  provider_id,
+  care_site_id,
+  visit_source_value,
+  visit_source_concept_id,
+  admitted_from_concept_id,
+  admitted_from_source_value,
+  discharged_to_concept_id,
+  discharged_to_source_value,
+  preceding_visit_occurrence_id
+from {{ source('omop', 'visit_occurrence') }} as vo
+inner join {{ ref('person') }} as p
+  on vo.person_id = p.person_id
