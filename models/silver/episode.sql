364061bce5d4a0fd2d51556ee6273205f7d38160
vvcb <vvcb.n1@gmail.com>
2024-02-12 09:44:17 +0000

    Add Episode to silver

---
+++ models/silver/episode.sql
@@ -0,0 +1,20 @@
+select
+    e.episode_id,
+    e.person_id,
+    e.episode_concept_id,
+    e.episode_start_date,
+    e.episode_start_datetime,
+    e.episode_end_date,
+    e.episode_end_datetime,
+    e.episode_parent_id,
+    e.episode_number,
+    e.episode_object_concept_id,
+    e.episode_type_concept_id,
+    e.episode_source_value,
+    e.episode_source_concept_id,
+    e.{{ adapter.quote("Version") }},
+    e.{{ adapter.quote("Year") }},
+    e.{{ adapter.quote("Month") }},
+    e.{{ adapter.quote("Day") }},
+    e.{{ adapter.quote("ProcessInstanceId") }}
+from {{ source('omop', 'episode') }} as e
